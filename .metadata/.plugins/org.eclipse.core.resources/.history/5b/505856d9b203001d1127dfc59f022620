c64393f56108697059c28ce8f625251a1a0f146d
Matias Basilotta <matiasbasilotta@gmail.com>
2022-07-23 17:40:27 -0300

    Haciendo pruebas

--- .metadata/.plugins/org.eclipse.core.resources/.history/5b/505856d9b203001d1127dfc59f022620
+++
@@ -1,4 +0,0 @@
-window.onload = function() {
-  console.log('+[window.onload]');
-  // Do nothing now
-};
--- BillMetal/widget/BillMetal/js/main.js
+++ BillMetal/widget/BillMetal/js/main.js
@@ -1,32 +1,40 @@
 window.onload = function() {
-  console.log('+[window.onload]');
-  // Do nothing now
-  const url = 'https://criptoya.com/api/dolar';
+    console.log('+[window.onload]');
+    // Do nothing now
+    const url = 'https://criptoya.com/api/dolar';
 
-  let respuesta= new XMLHttpRequest();
-  respuesta.open('GET',url);
+    
 
-  respuesta.onreadystatechange=function(){
-     
-      if(respuesta.readyState === XMLHttpRequest.DONE){
-          
-          let respuestaJson= JSON.parse(this.responseText);
-          console.log(respuestaJson);
-          document.getElementById('oficial').innerText = respuestaJson.oficial;
-          document.getElementById('blue').innerText= respuestaJson.blue;
-          var date=new Date();
-          document.getElementById('hora').innerText= "Actualizado "+formatoHora(date.getHours())+":"+formatoHora(date.getMinutes())+":"+formatoHora(date.getSeconds());
-      }
-  };
-  respuesta.send();
-  
-  function formatoHora(hora){
-	  if(hora<10){
-		  return "0"+hora;
-	  }else{
-		  return hora;
-	  }
-  }
-  
+        let respuesta = new XMLHttpRequest();
+        respuesta.open('GET', url);
+
+        respuesta.onreadystatechange = function() {
+
+            if (respuesta.readyState === XMLHttpRequest.DONE) {
+
+                let respuestaJson = JSON.parse(this.responseText);
+                console.log(respuestaJson);
+                document.getElementById('oficial').innerText = respuestaJson.oficial;
+                document.getElementById('blue').innerText = respuestaJson.blue;
+                var date = new Date();
+                document.getElementById('hora').innerText = "Actualizado " + formatoHora(date.getHours()) + ":" + formatoHora(date.getMinutes()) + ":" + formatoHora(date.getSeconds());
+            }
+        };
+        respuesta.send();
+
+    
+
+    function formatoHora(hora) {
+        if (hora < 10) {
+            return "0" + hora;
+        } else {
+            return hora;
+        }
+    }
+
+   
+    
+
+
 };
 
