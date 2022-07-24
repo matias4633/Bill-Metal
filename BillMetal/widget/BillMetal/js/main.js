1bfcf215b58a25cb1880fe450b2519e724ad7eb1
Matias Basilotta <matiasbasilotta@gmail.com>
2022-07-24 02:07:55 -0300

    agregado de funcion offline

--- BillMetal/widget/BillMetal/js/main.js
+++ BillMetal/widget/BillMetal/js/main.js
@@ -1,28 +1,38 @@
 window.onload = function() {
-    console.log('+[window.onload]');
+    //console.log('+[window.onload]');
     // Do nothing now
     const url = 'https://criptoya.com/api/dolar';
 
-    
+    var ultimaHora = new Date();
+    var hora;
+
+    function contenido() {
+
 
         let respuesta = new XMLHttpRequest();
+
         respuesta.open('GET', url);
+
+        //console.log(respuesta.readyState);
 
         respuesta.onreadystatechange = function() {
 
-            if (respuesta.readyState === XMLHttpRequest.DONE) {
+            if (respuesta.readyState === XMLHttpRequest.DONE && respuesta.status===200) {
 
-                let respuestaJson = JSON.parse(this.responseText);
-                console.log(respuestaJson);
+            	localStorage.setItem(hora, formatoHora(ultimaHora.getHours()) + ":" + formatoHora(ultimaHora.getMinutes()) + ":" + formatoHora(ultimaHora.getSeconds()));
+            	let respuestaJson = JSON.parse(this.responseText);
+                //console.log(respuestaJson);
                 document.getElementById('oficial').innerText = respuestaJson.oficial;
                 document.getElementById('blue').innerText = respuestaJson.blue;
-                var date = new Date();
-                document.getElementById('hora').innerText = "Actualizado " + formatoHora(date.getHours()) + ":" + formatoHora(date.getMinutes()) + ":" + formatoHora(date.getSeconds());
+              
+                document.getElementById('hora').innerText = "Actualizado " + localStorage.getItem(hora);
+            }else{
+            	document.getElementById('hora').innerText = "Ultima actualizacion: " + localStorage.getItem(hora) + "\nConexión no disponible.";
             }
         };
         respuesta.send();
 
-    
+    }
 
     function formatoHora(hora) {
         if (hora < 10) {
@@ -32,9 +42,12 @@ window.onload = function() {
         }
     }
 
-   
     
 
+    contenido();
+    
+    
+    
 
 };
 
